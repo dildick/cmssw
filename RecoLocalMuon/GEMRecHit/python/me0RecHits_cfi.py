c0a737179262ce344c98d5ed47f95eb0d80192e1
Sven Dildick <sven.dildick@cern.ch>
2018-01-04 01:13:15 +0100

    fix

--- RecoLocalMuon/GEMRecHit/python/me0RecHits_cfi.py
+++ RecoLocalMuon/GEMRecHit/python/me0RecHits_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-me0PseudoRecHits = cms.EDProducer("ME0RecHitProducer",
+me0RecHits = cms.EDProducer("ME0RecHitProducer",
     recAlgoConfig = cms.PSet(),
     recAlgo = cms.string('ME0RecHitStandardAlgo'),
     me0DigiLabel = cms.InputTag("simMuonME0Digis"),
