2578747aab650af05d477a864f828cf9b531e2d6
Sven Dildick <sven.dildick@cern.ch>
2018-01-04 02:15:39 +0100

    fix

--- RecoLocalMuon/GEMRecHit/src/ME0RecHitProducer.cc
+++ RecoLocalMuon/GEMRecHit/src/ME0RecHitProducer.cc
@@ -23,7 +23,7 @@ ME0RecHitProducer::ME0RecHitProducer(const ParameterSet& config)
   // Set verbose output
   produces<ME0RecHitCollection>();
 
-  theME0DigiToken = consumes<ME0DigiCollection>(config.getParameter<edm::InputTag>("gemDigiLabel"));
+  theME0DigiToken = consumes<ME0DigiCollection>(config.getParameter<edm::InputTag>("me0DigiLabel"));
 
   // Get the concrete reconstruction algo from the factory
   string theAlgoName = config.getParameter<string>("recAlgo");
@@ -46,8 +46,8 @@ void ME0RecHitProducer::beginRun(const edm::Run& r, const edm::EventSetup& setup
 void ME0RecHitProducer::produce(Event& event, const EventSetup& setup)
 {
   // Get the ME0 Geometry
-  ESHandle<ME0Geometry> gemGeom;
-  setup.get<MuonGeometryRecord>().get(gemGeom);
+  ESHandle<ME0Geometry> me0Geom;
+  setup.get<MuonGeometryRecord>().get(me0Geom);
 
   // Get the digis from the event
   Handle<ME0DigiCollection> digis;
@@ -60,27 +60,27 @@ void ME0RecHitProducer::produce(Event& event, const EventSetup& setup)
   auto recHitCollection = std::make_unique<ME0RecHitCollection>();
 
   // Iterate through all digi collections ordered by LayerId
-  ME0DigiCollection::DigiRangeIterator gemdgIt;
-  for (gemdgIt = digis->begin(); gemdgIt != digis->end();
-       ++gemdgIt){
+  ME0DigiCollection::DigiRangeIterator me0dgIt;
+  for (me0dgIt = digis->begin(); me0dgIt != digis->end();
+       ++me0dgIt){
 
     // The layerId
-    const ME0DetId& gemId = (*gemdgIt).first;
+    const ME0DetId& me0Id = (*me0dgIt).first;
 
     // Get the GeomDet from the setup
-    const ME0EtaPartition* roll = gemGeom->etaPartition(gemId);
+    const ME0EtaPartition* roll = me0Geom->etaPartition(me0Id);
 
     // Get the iterators over the digis associated with this LayerId
-    const ME0DigiCollection::Range& range = (*gemdgIt).second;
+    const ME0DigiCollection::Range& range = (*me0dgIt).second;
 
     // Getting the roll mask, that includes dead strips, for the given ME0Det
     ME0EtaPartitionMask mask;
 
     // Call the reconstruction algorithm
-    OwnVector<ME0RecHit> recHits = theAlgo->reconstruct(*roll, gemId, range, mask);
+    OwnVector<ME0RecHit> recHits = theAlgo->reconstruct(*roll, me0Id, range, mask);
 
     if(!recHits.empty())
-      recHitCollection->put(gemId, recHits.begin(), recHits.end());
+      recHitCollection->put(me0Id, recHits.begin(), recHits.end());
   }
 
   event.put(std::move(recHitCollection));
