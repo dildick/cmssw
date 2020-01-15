001f123822d48b52e80dfac7709dccf1950160d3
Sven Dildick <sven.dildick@cern.ch>
2020-01-15 16:07:46 +0100

    Pos LUT

--- L1Trigger/CSCTriggerPrimitives/interface/CSCComparatorCodePosLUT.h
+++ L1Trigger/CSCTriggerPrimitives/interface/CSCComparatorCodePosLUT.h
@@ -5,26 +5,39 @@
 #include <sstream>
 #include <bitset>
 
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "CondFormats/L1TObjects/interface/LUT.h"
 
-class CSCComparatorCodePosLUT : public LUT {
+class CSCComparatorCodePosLUT : public l1t::LUT
+{
  public:
   CSCComparatorCodePosLUT(){};
   explicit CSCComparatorCodePosLUT(const std::string&);
   explicit CSCComparatorCodePosLUT(l1t::LUT*);
-  ~CSCComparatorCodePosLUT() override{};
+  ~CSCComparatorCodePosLUT() {}
 
   int lookup(int pat, int code) const;
-  int lookupPacked(int in) const override;
+  int lookupPacked(int in) const;
+
+  // populates the map.
+  void initialize();
 
   int hashInput(int pt, int code) const;
   void unHashInput(int input, int& pat, int& code) const;
 
   int checkedInput(unsigned in, unsigned maxWidth) const;
 
+  // I/O functions
+  void save(std::ofstream& output);
+  int load(const std::string& inFileName);
+
  private:
+  unsigned m_totalInWidth;
+  unsigned m_outWidth;
   int m_patInWidth;
   int m_codeInWidth;
+  bool m_initialized;
 };
 
 #endif
